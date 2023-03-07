964cd1cc61f645cb543e2af16a247094e6bf60d5
Aldair Zamora <aldairzamora@microsoft.com>
2023-03-07 10:20:37 -0600

    complete expressions topic

---
+++ LearnCppTutorial/Chapter1/IntroductionsToExpressions/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int five()
+{
+    return 5;
+}
+
+int main()
+{
+    // expressions returns s single value after an evaluation
+    int x{2 + 5}; // 2+5 is the expression and evaluates to 7 |  type identifier { expression }
+
+    return 0;
+}
